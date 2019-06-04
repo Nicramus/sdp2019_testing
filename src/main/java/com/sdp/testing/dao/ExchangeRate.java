82aff42cd2c31ac08ff837d88c2277d922d32f8b
marcin.mielcarski <larry.marcin@gmail.com>
2019-06-04 08:12:26 +0200

    first commit

---
+++ src/main/java/com/sdp/testing/dao/ExchangeRate.java
@@ -0,0 +1,27 @@
+package com.sdp.testing.dao;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class ExchangeRate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String key;
+    private BigDecimal rate;
+
+    public ExchangeRate(){}
+
+    public ExchangeRate(String key, BigDecimal rate) {
+        this.key = key;
+        this.rate = rate;
+    }
+}
